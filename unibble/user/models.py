34b747c734ead758649337c0b5a3e479fc487818
최정호 <acepilot06200620@gmail.com>
2021-11-10 19:02:53 +0900

    superuser as custom user

--- unibble/user/models.py
+++ unibble/user/models.py
@@ -41,6 +41,7 @@ class User(AbstractUser):
     email = models.EmailField(_('email address'), unique=True)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    objects = UserManager()
 class Unibber(models.Model):
     MAJOR = [
         ("de","Default"),
